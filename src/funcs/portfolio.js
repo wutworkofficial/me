452d896c77afacf98751667afe5257e70c4a65a3
nuttawut-kpc <72194786+nuttawut-kpc@users.noreply.github.com>
2025-09-24 23:53:19 +0700

    order slide

--- src/funcs/portfolio.js
+++ src/funcs/portfolio.js
@@ -6,7 +6,7 @@ export const handleVideoPlayerControls = () => {
   const target = document.querySelector('.my-portfolio-1')
   const isActive = target.classList.contains('active')
 
-  const delay = 2000
+  const delay = 3000
 
   if (isActive && !player.played()) {
     timeout = setTimeout(() => player.play(), delay)
